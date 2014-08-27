64d21e2978c0ed30f2731fd991400c6ff1080066
praveensagarc <praveensagarc@gmail.com>
2014-08-27 10:17:59 +0530

    For add bm

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,4 +2,8 @@ class BookmarksController < ApplicationController
   def index
 	@bookmarks = Bookmark.all
   end
+  def add
+   Bookmark.create(:bookmarkName => params[:bookmark_text])
+   redirect_to :action => 'index'
+  end
 end
